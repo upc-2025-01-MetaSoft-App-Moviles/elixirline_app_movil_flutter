1fd7a0f9c866514a24dced54a85b774f8179e2d4
petitavo <gustavohuanca1020@gmail.com>
2025-06-19 01:01:34 -0500

    first commit

---
+++ android/app/src/main/kotlin/pe/edu/metasoft/elixirline_app_movil_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package pe.edu.metasoft.elixirline_app_movil_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Nueva App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('¡Hola mundo!'),
+      ),
+    );
+  }
+}
